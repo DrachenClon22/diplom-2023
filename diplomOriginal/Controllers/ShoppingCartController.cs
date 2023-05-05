1a131c57225af4c7030e7e1694e29f5d2e1f22f5
DrachenClon22 <drachenclon22@gmail.com>
2023-05-05 14:20:05 +0300

    Added items to json list

---
+++ diplomOriginal/Controllers/ShoppingCartController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace diplomOriginal.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        public IActionResult Index()
+        {
+            var user = HttpContext.User.Identity?.IsAuthenticated;
+            if (user is not null && user.Value)
+            {
+                return View();
+            }
+            else
+            {
+                return Redirect("/");
+            }
+        }
+    }
+}
